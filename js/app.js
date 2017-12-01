ff96f818c7502e611ff551561167180a496e29c9
langersp <langleyp@hotmail.com>
2017-12-01 19:37:26 +0000

    refactor app.js

--- js/app.js
+++ js/app.js
@@ -2,16 +2,36 @@ import { Basket } from './classes/basket';
 import { Item } from './classes/item';
 import { pricingRules } from './pricingRules';
 
+const productData = [
+	{
+		'code': 'FR1',
+		'name': 'Fruit tea',
+		'image': 'https://www.gourmetgiftbaskets.com/Blog/wp-content/uploads/2015/7/fruit-tea-2015.jpg',
+		'price': 3.11,
+		'promotion': { 
+			'code': 'bogof', 
+			'name': 'Buy One Get One Free'
+		}
+	},
+	{
+		'code': 'SR1',
+		'name': 'Strawberries',
+		'image': 'https://www.organicfacts.net/wp-content/uploads/2013/06/Strawberry1-1020x765.jpg',
+		'price': 5.00,
+		'promotion': {
+			'code': 'pricediscount',
+			'name': 'Buy 3 Get 50p off Each'
+		}
+	},
+	{
+		'code': 'CF1',
+		'name': 'Coffee',
+		'image': 'https://i2-prod.mirror.co.uk/incoming/article10381392.ece/ALTERNATES/s1200/PROD-Coffee-and-beans.jpg',
+		'price': 11.23
+	}
+];
 
 const basket = new Basket(pricingRules);
-
-//creat new items and show on UI
-const item1 = new Item('FR1', 'Fruit tea', 'https://www.gourmetgiftbaskets.com/Blog/wp-content/uploads/2015/7/fruit-tea-2015.jpg', 3.11, { code:'bogof', name:'Buy One Get One Free'});
-item1.displayItem();
-const item2 = new Item('SR1', 'Strawberries', 'https://www.organicfacts.net/wp-content/uploads/2013/06/Strawberry1-1020x765.jpg', 5.00, { code:'pricediscount', name:'Buy 3 Get 50p off Each'});
-item2.displayItem();
-const item3 = new Item('CF1', 'Coffee', 'https://i2-prod.mirror.co.uk/incoming/article10381392.ece/ALTERNATES/s1200/PROD-Coffee-and-beans.jpg', 11.23);
-item3.displayItem();
 
 const clickHandler = (item) => {
 	basket.add(item);
@@ -19,12 +39,8 @@ const clickHandler = (item) => {
 	basket.displayBasket();
 }
 
-item1.attachEventListener(clickHandler);
-item2.attachEventListener(clickHandler);
-item3.attachEventListener(clickHandler);
-
-
-
-
-
-
+productData.forEach(item => {
+	let productItem = new Item(item.code, item.name, item.image, item.price, item.promotion);
+	productItem.displayItem();
+    productItem.attachEventListener(clickHandler);
+});
